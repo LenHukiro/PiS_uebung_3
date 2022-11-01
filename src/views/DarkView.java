a61426b25a3e73560d9035ae70b3b28aed5bada5
Niklas <Niklas89@t-online.de>
2022-11-01 14:22:11 +0100

    started overriding setup from Light/DarkView

--- src/views/DarkView.java
+++ src/views/DarkView.java
@@ -6,4 +6,18 @@ public class DarkView  extends BaseView {
     public static void main(String... args) {
         PApplet.runSketch(new String[]{""}, new DarkView());
     }
+
+
+    @Override
+    public void setup() {
+        textAlign(CENTER, CENTER);
+        textSize(27);
+        noStroke();
+        background(color(179, 189, 214));
+        colorMode(HSB, 360, 100, 100);
+
+        controller.random_tile();
+        controller.random_tile();
+        show();
+    }
 }
--- src/views/LightView.java
+++ src/views/LightView.java
@@ -7,4 +7,17 @@ public class LightView extends BaseView {
     public static void main(String... args) {
         PApplet.runSketch(new String[]{""}, new LightView());
     }
+
+    @Override
+    public void setup() {
+        textAlign(CENTER, CENTER);
+        textSize(27);
+        noStroke();
+        background(color(179, 189, 214));
+        colorMode(HSB, 360, 100, 100);
+
+        controller.random_tile();
+        controller.random_tile();
+        show();
+    }
 }
