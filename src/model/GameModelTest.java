4ef1769bd76b4216615bd29a7afca012b403f0df
Niklas <Niklas89@t-online.de>
2022-11-11 11:02:56 +0100

    Added GameModelTest

---
+++ src/model/GameModelTest.java
@@ -0,0 +1,75 @@
+package model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameModelTest {
+
+    GameModel model;
+    @BeforeEach
+    void setUp() {
+        model = new GameModel();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void merge() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void insert_tile() {
+    }
+
+    @Test
+    void random_tile() {
+    }
+
+    @Test
+    void free_slots() {
+    }
+
+    @Test
+    void is_game_over() {
+    }
+
+    @Test
+    void shift() {
+    }
+
+    @Test
+    void rotate() {
+    }
+
+    @Test
+    void testRotate() {
+    }
+
+    @Test
+    void getGrid() {
+    }
+
+    @Test
+    void getScore() {
+    }
+
+    @Test
+    void addToScore() {
+    }
+
+    @Test
+    void gameOver() {
+        model.gameOver();
+    }
+
+    @Test
+    void isGameRunning() {
+    }
+}
